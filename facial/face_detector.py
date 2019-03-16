a7e2564eca1c029bf5486100b4d0d2d603901ffa
Smascar <sanford.6458@gmail.com>
2019-03-16 11:39:29 +0530

    Function Separation

--- facial/face_detector.py
+++ facial/face_detector.py
@@ -14,23 +14,7 @@ class detector:
 
     def get_positions(self,img):
         if self.algorithm == 'haar':
-            #path_img = "demo_pic.jpg"#sys.argv[1]
-            path_frontfacecascade = "haarcascade_frontalface_default.xml"
-            # Create Model 
-            model = cv2.CascadeClassifier(path_frontfacecascade)
-            # Import image 
-            #img = cv2.imread(path_img)
-            
-            # Gray conversion for haar requirement
-            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
-            # faces will store the array of boxes of positions
-            faces = model.detectMultiScale(
-                img_gray,
-                scaleFactor=1.1,
-                minNeighbors=5,
-                minSize=(1,1),
-                flags = cv2.CASCADE_SCALE_IMAGE
-            )
+            faces = self.haar_cascader(img)
             
         return faces 
 
