7500ccc1ed5f57f9d7ec6afdb819b5a3f974eb32
Smascar <sanford.6458@gmail.com>
2019-03-16 11:53:27 +0530

    Documentation

--- facial/face_detector.py
+++ facial/face_detector.py
@@ -13,23 +13,44 @@ class detector:
         self.algorithm = algorithm
 
     def get_positions(self,img):
+        '''
+        Gets the bounding boxes positions.
+
+        This is a wrapper function for all the algorithms in this list, 
+        each algorithm can be individually accessed however.
+        Main purpose is for ease of use.
+        Select algorithm during init. 
+        Default to haar.
+
+        Parameters:
+        arg1 (numpy.ndarray): image to work on
+
+        Returns: 
+        numpy.ndarray: Bounding boxes positions
+        '''
         if self.algorithm == 'haar':
             faces = self.haar_cascader(img)
             
         return faces 
 
     def haar_cascader(self,img):
-        #path_img = "demo_pic.jpg"#sys.argv[1]
-        path_frontfacecascade = "haarcascade_frontalface_default.xml"
-        # Create Model 
-        model = cv2.CascadeClassifier(path_frontfacecascade)
-        # Import image 
-        #img = cv2.imread(path_img)
+        '''
+        Gets the bounding boxes positions.
+
+        Here, haar cascade is specifically used.
+
+        Parameters:
+        arg1 (numpy.ndarray): image to work on
+
+        Returns: 
+        numpy.ndarray: Bounding boxes positions
+        '''
+        path_frontfacecascade = "haarcascade_frontalface_default.xml"   # Get the xml
+        model = cv2.CascadeClassifier(path_frontfacecascade)            # Create Model 
         
-        # Gray conversion for haar requirement
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
-        # faces will store the array of boxes of positions
-        faces = model.detectMultiScale(
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)                # Gray conversion for haar requirement
+
+        faces = model.detectMultiScale(                                 # faces will store the array of boxes of positions
             img_gray,
             scaleFactor=1.1,
             minNeighbors=5,
