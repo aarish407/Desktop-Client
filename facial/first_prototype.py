99a9a107c86056ca73630897b4675481a3bb516a
Smascar <sanford.6458@gmail.com>
2019-03-16 10:45:29 +0530

    First Commit

---
+++ facial/first_prototype.py
@@ -0,0 +1,16 @@
+# algo to detect face positions and bounding boxes positions
+# For now take haar cascade for quick prototyping
+# Save the bounding boxes
+
+# Haar Cascade 
+
+import cv2
+import sys
+
+
+
+# for each face you found
+    # crop face and align it
+    # send to parameter calculation algorithm 
+    # save parameters to dataset(pandas dataframe)
+    # (Later Stage) Calculate Clusters and all
